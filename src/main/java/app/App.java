96e5e59fe05088b1a75bbac661c243c15bf2cac9
hyun <zcvfcat@gmail.com>
2019-11-06 10:03:12 +0900

    jjh git

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -5,22 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
-
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
-
 }
-
-// @Autowired
-// RedisTemplate<String, Object> redisTemplate;
-
-// @Bean
-// ApplicationRunner test() {
-// return args -> {
-// ValueOperations<String, Object> values = redisTemplate.opsForValue();
-// values.set("test", "test");
-// values.set("java", "java");
-// values.set("c", "c");
-// };
-// }
